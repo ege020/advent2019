6a75e59975794a6fc281a20b093d83dd13cca7c6
ege020 <etezcan@deloitte.nl>
2019-12-17 13:00:44 +0100

    commit on master

--- challenge1.py
+++ challenge1.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 filepath = 'challenge1.txt'
 
 with open(filepath) as fp:
@@ -14,21 +13,4 @@ with open(filepath) as fp:
        line = fp.readline()
        cnt += 1
 
-=======
-filepath = 'challenge1.txt'
-
-with open(filepath) as fp:
-   line = fp.readline()
-   cnt = 1
-   total = 0
-   while line:
-       fuel = (int(line)//3)-2
-       total += fuel
-       if fuel > 6:
-          line = fuel
-          continue
-       line = fp.readline()
-       cnt += 1
-
->>>>>>> ba80abafa32e5830856c4f742bd5eafc5ce3bbc9
 print(total)
